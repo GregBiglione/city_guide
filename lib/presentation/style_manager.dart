2091b73bb18f6fecb8f55719dbff5d287d232263
GregBiglione <gregory.biglione81@gmail.com>
2023-07-12 01:18:49 +0200

    Added AppBarTheme in ThemeManager

--- lib/presentation/style_manager.dart
+++ lib/presentation/style_manager.dart
@@ -1,4 +1,4 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 import 'package:city_guide/presentation/font_manager.dart';
 
--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,4 +1,6 @@
 import 'package:city_guide/presentation/color_manager.dart';
+import 'package:city_guide/presentation/font_manager.dart';
+import 'package:city_guide/presentation/style_manager.dart';
 import 'package:city_guide/presentation/value_manager.dart';
 import 'package:flutter/material.dart';
 
@@ -11,13 +13,26 @@ ThemeData getApplicationTheme() {
     disabledColor: ColorManager.grey1,
     colorScheme: ColorScheme.fromSwatch().copyWith(secondary: ColorManager.grey),
 
+    // Ripple theme ---------------------------------------------------------
+    splashColor: ColorManager.primaryOpacity70,
     // Card view theme ---------------------------------------------------------
     cardTheme: CardTheme(
       color: ColorManager.white,
       shadowColor: ColorManager.grey,
       elevation: AppSize.s4,
     ),
+
     // App bar theme -----------------------------------------------------------
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
+        color: ColorManager.white,
+        fontSize: FontSize.s16,
+      ),
+    ),
     // Button theme ------------------------------------------------------------
     // Text theme --------------------------------------------------------------
     // Input decoration theme text form field ----------------------------------
