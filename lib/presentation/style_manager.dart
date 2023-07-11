ef8eca4c25a028e44d87c87680ae4c0cac9f0a89
GregBiglione <gregory.biglione81@gmail.com>
2023-07-11 23:36:13 +0200

    Added StyleManager

---
+++ lib/presentation/style_manager.dart
@@ -0,0 +1,67 @@
+import 'dart:ui';
+
+import 'package:city_guide/presentation/font_manager.dart';
+
+TextStyle _getTextStyle(double fontSize, String fontFamily,
+    FontWeight fontWeight, Color color) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontFamily: fontFamily,
+    color: color,
+  );
+}
+
+// Light style -----------------------------------------------------------------
+
+TextStyle getLightStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.light,
+    color,
+  );
+}
+
+// Regular style ---------------------------------------------------------------
+
+TextStyle getRegularStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.regular,
+    color,
+  );
+}
+
+// Medium style ----------------------------------------------------------------
+
+TextStyle getMediumStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.medium,
+    color,
+  );
+}
+
+// Semi bold style -------------------------------------------------------------
+
+TextStyle getSemiBoldStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.semiBold,
+    color,
+  );
+}
+
+// Bold style ------------------------------------------------------------------
+
+TextStyle getBoldStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManager.bold,
+    color,
+  );
+}
