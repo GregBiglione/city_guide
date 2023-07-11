17972055e47053cc723c4e4e7908564b298f2330
GregBiglione <gregory.biglione81@gmail.com>
2023-07-11 23:59:13 +0200

    Added ThemeManager & colors in ColorManager

---
+++ lib/presentation/theme_manager.dart
@@ -0,0 +1,19 @@
+import 'package:city_guide/presentation/color_manager.dart';
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    // Colors of the app -------------------------------------------------------
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: ColorManager.grey),
+    // Card view theme ---------------------------------------------------------
+    // App bar theme -----------------------------------------------------------
+    // Button theme ------------------------------------------------------------
+    // Text theme --------------------------------------------------------------
+    // Input decoration theme text form field ----------------------------------
+
+  );
+}
