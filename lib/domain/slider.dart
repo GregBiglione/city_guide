e58f5e071e01672893f6cea992e840483f218b26
GregBiglione <gregory.biglione81@gmail.com>
2023-07-17 22:59:07 +0200

    Added Slider object & added SliderViewObject in OnBoardingViewModel

---
+++ lib/domain/slider.dart
@@ -0,0 +1,7 @@
+class Slider {
+  String title;
+  String subTitle;
+  String image;
+
+  Slider(this.title, this.subTitle, this.image);
+}
--- lib/presentation/on_boarding/on_boarding_view_model.dart
+++ lib/presentation/on_boarding/on_boarding_view_model.dart
@@ -1,7 +1,13 @@
+import 'dart:async';
+
+import 'package:city_guide/domain/slider.dart';
 import 'package:city_guide/presentation/base/base_view_model.dart';
+import 'package:city_guide/presentation/on_boarding/on_boarding_view.dart';
 
 class OnBoardingViewModel extends BaseViewModel with OnBoardingViewModelInput,
     OnBoardingViewModelOutput {
+  final StreamController _streamController = StreamController<SliderViewObject>();
+
   @override
   void dispose() {
     // TODO: implement dispose
@@ -20,6 +26,14 @@ class OnBoardingViewModel extends BaseViewModel with OnBoardingViewModelInput,
 
   @override
   void onPageChanged(int index) {}
+
+  @override
+  // TODO: implement inputSliderViewObject
+  Sink get inputSliderViewObject => throw UnimplementedError();
+
+  @override
+  // TODO: implement outputSliderViewObject
+  Stream<SliderViewObject> get outputSliderViewObject => throw UnimplementedError();
 }
 
 // Input means order that view model will receive from view --------------------
@@ -27,7 +41,19 @@ abstract class OnBoardingViewModelInput {
   void goPrevious() {}
   void goNext() {}
   void onPageChanged(int index) {}
+
+  Sink get inputSliderViewObject;
 }
 
 // Output means data/result that will be sent from view to view ----------------
-abstract class OnBoardingViewModelOutput {}
+abstract class OnBoardingViewModelOutput {
+  Stream<SliderViewObject> get outputSliderViewObject;
+}
+
+class SliderViewObject {
+  Slider slider;
+  int numberOfSlides;
+  int currentIndex;
+
+  SliderViewObject(this.slider, this.numberOfSlides, this.currentIndex);
+}
