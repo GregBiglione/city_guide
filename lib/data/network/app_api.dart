a06cad399e221ca3912b77d62cddcffa76d05cbb
GregBiglione <gregory.biglione81@gmail.com>
2023-07-18 23:50:36 +0200

    Added AppServiceClient, app_api.g & Constant

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,15 @@
+import 'package:city_guide/app/constant.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+
+import '../response/response.dart';
+
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constant.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+  
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login();
+}
