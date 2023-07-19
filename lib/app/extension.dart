b82fc26fd1a45ab6d3fe2d9bd54c72ef50d9ccb1
GregBiglione <gregory.biglione81@gmail.com>
2023-07-19 20:14:04 +0200

    Added NonNullString & NonNullInt

---
+++ lib/app/extension.dart
@@ -0,0 +1,23 @@
+// String extension ------------------------------------------------------------
+extension NonNullString on String? {
+  String orEmpty() {
+    if(this == null) {
+      return "";
+    }
+    else {
+      return this!;
+    }
+  }
+}
+
+// int extension ---------------------------------------------------------------
+extension NonNullInt on int? {
+  int orZero() {
+    if(this == null) {
+      return 0;
+    }
+    else {
+      return this!;
+    }
+  }
+}
