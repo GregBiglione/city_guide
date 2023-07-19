d2e1bcebf2e0292340b602590b5c46214babf709
GregBiglione <gregory.biglione81@gmail.com>
2023-07-19 19:53:50 +0200

    Added Customer, Contacts & Authentication objects

---
+++ lib/domain/authentication.dart
@@ -0,0 +1,9 @@
+import 'contacts.dart';
+import 'customer.dart';
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(this.customer, this.contacts);
+}
---
+++ lib/domain/customer.dart
@@ -0,0 +1,7 @@
+class Customer {
+  String id;
+  String name;
+  String numberOfNotification;
+
+  Customer(this.id, this.name, this.numberOfNotification);
+}
