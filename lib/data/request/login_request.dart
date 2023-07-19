e5d249c1b4a4cff49385f9ba31dbf035a454b567
GregBiglione <gregory.biglione81@gmail.com>
2023-07-19 21:31:39 +0200

    Added LoginRequest & Failure objects & Repository

---
+++ lib/data/request/login_request.dart
@@ -0,0 +1,8 @@
+class LoginRequest {
+  String username;
+  String password;
+  String imei;
+  String deviceType;
+
+  LoginRequest(this.username, this.password, this.imei, this.deviceType);
+}
---
+++ lib/domain/repository.dart
@@ -0,0 +1,9 @@
+import 'package:city_guide/data/request/login_request.dart';
+import 'package:dartz/dartz.dart';
+
+import '../data/network/failure.dart';
+import 'authentication.dart';
+
+abstract class Repository {
+  Future<Either<Failure, Authentication>> login(LoginRequest loginRequest);
+}
