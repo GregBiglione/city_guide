4237239ab5092021c900eb40b26e49e61d1f9e16
GregBiglione <gregory.biglione81@gmail.com>
2023-07-20 22:01:06 +0200

    Added ApiInternalStatus & modified RepositoryImplementer

---
+++ lib/data/network/api_internal_status.dart
@@ -0,0 +1,4 @@
+class ApiInternalStatus {
+  static const int SUCCESS = 0;
+  static const int FAILURE = 1;
+}
--- lib/data/repository/repository_implementer.dart
+++ lib/data/repository/repository_implementer.dart
@@ -1,5 +1,11 @@
 import 'package:city_guide/data/mapper/mapper.dart';
+import 'package:city_guide/data/network/api_internal_status.dart';
+import 'package:city_guide/data/network/error_data_source.dart';
+import 'package:city_guide/data/network/error_data_source_extension.dart';
+import 'package:city_guide/data/network/error_handler.dart';
 import 'package:city_guide/data/network/failure.dart';
+import 'package:city_guide/data/network/response_code.dart';
+import 'package:city_guide/data/network/response_message.dart';
 
 import 'package:city_guide/data/request/login_request.dart';
 
@@ -20,26 +26,30 @@ class RepositoryImplementer implements Repository {
   @override
   Future<Either<Failure, Authentication>> login(LoginRequest loginRequest) async {
     if(await _networkInfo.isConnected) {
-      // It(s safe to call the API ---------------------------------------------
-      final response = await _remoteDataSource.login(loginRequest);
+      try {
+        // It(s safe to call the API ---------------------------------------------
+        final response = await _remoteDataSource.login(loginRequest);
 
-      if(response.status == 0) {
+        if(response.status == ApiInternalStatus.SUCCESS) {
         // Ok ------------------------------------------------------------------
-        return Right(response.toDomain());
-      }
-      else {
-        // Business logic error ------------------------------------------------
-        return Left(
-            Failure(
-                409,
-                response.message ?? "We have business logic error from API side"
-            ),
-        );
+          return Right(response.toDomain());
+        }
+        else {
+          // Business logic error ------------------------------------------------
+          return Left(
+              Failure(
+                response.status ?? ApiInternalStatus.FAILURE,
+                response.message ?? ResponseMessage.DEFAULT
+              ),
+          );
+        }
+      } catch (e) {
+        return Left(ErrorHandler.handle(e).failure);
       }
     }
     else {
       // Connection error ------------------------------------------------------
-      return Left(Failure(501, "Please check your internet connection"));
+      return Left(ErrorDataSource.NO_INTERNET_CONNECTION.getFailure());
     }
   }
 }
