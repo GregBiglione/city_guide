7988fd4f716c4db6e240a15e412da7767efab413
GregBiglione <gregory.biglione81@gmail.com>
2023-07-26 19:25:29 +0200

    Added FlowStateExtension

--- lib/presentation/common/state_renderer/state_renderer_implementer.dart
+++ lib/presentation/common/state_renderer/state_renderer_implementer.dart
@@ -1,6 +1,7 @@
 import 'package:city_guide/data/mapper/mapper.dart';
 import 'package:city_guide/presentation/common/state_renderer/state_renderer.dart';
 import 'package:city_guide/presentation/ressource/string_manager.dart';
+import 'package:flutter/material.dart';
 
 abstract class FlowState {
   StateRenderType getStateRenderType();
@@ -59,4 +60,69 @@ class EmptyState extends FlowState {
 
   @override
   StateRenderType getStateRenderType() => StateRenderType.EMPTY_SCREEN_STATE;
+}
+
+extension FlowStateExtension on FlowState {
+  Widget _getWidgetScreen(BuildContext context, Widget contentScreenWidget,
+      Function retryActionFunction) {
+    switch(runtimeType) {
+      case LoadingState: {
+        if(getStateRenderType() == StateRenderType.POPUP_LOADING_STATE) {
+          // Showing popup dialog ----------------------------------------------
+          showPopUp(context, getStateRenderType(), getMessage());
+          // Return the content ui of the screen -------------------------------
+          return contentScreenWidget;
+        }
+        else {
+          return StateRender(
+            stateRenderType: getStateRenderType(),
+            retryActionFunction: retryActionFunction,
+            message: getMessage(),
+          );
+        }
+      }
+      case ErrorState: {
+        if(getStateRenderType() == StateRenderType.POPUP_ERROR_STATE) {
+          // Showing popup dialog ----------------------------------------------
+          showPopUp(context, getStateRenderType(), getMessage());
+          // Return the content ui of the screen -------------------------------
+          return contentScreenWidget;
+        }
+        else {
+          return StateRender(
+            stateRenderType: getStateRenderType(),
+            retryActionFunction: retryActionFunction,
+            message: getMessage(),
+          );
+        }
+      }
+      case ContentState: {
+        return contentScreenWidget;
+      }
+      case EmptyState: {
+        return StateRender(
+          stateRenderType: getStateRenderType(),
+          retryActionFunction: retryActionFunction,
+          message: getMessage(),
+        );
+      }
+      default: {
+        return contentScreenWidget;
+      }
+    }
+  }
+
+  showPopUp(BuildContext context, StateRenderType stateRenderType,
+      String message) {
+    WidgetsBinding.instance.addPostFrameCallback((_) =>
+        showDialog(
+          context: context,
+          builder: (BuildContext context) => StateRender(
+            stateRenderType: stateRenderType,
+            message: message,
+            retryActionFunction: (){},
+          ),
+        ),
+    );
+  }
 }
