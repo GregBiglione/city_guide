9e00a3249ade6f9db07140c46b7d2b173465d4b0
GregBiglione <gregory.biglione81@gmail.com>
2023-08-01 20:32:07 +0200

    Modified newPassword() in Repository

--- lib/domain/repository/repository.dart
+++ lib/domain/repository/repository.dart
@@ -3,9 +3,10 @@ import 'package:city_guide/data/request/new_password_request.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../data/network/failure.dart';
+import '../../data/response/forgot_password_response.dart';
 import '../model/authentication.dart';
 
 abstract class Repository {
   Future<Either<Failure, Authentication>> login(LoginRequest loginRequest);
-  Future<Either<Failure, String>> newPassword(NewPasswordRequest newPasswordRequest);
+  Future<Either<Failure, ForgotPasswordResponse>> newPassword(NewPasswordRequest newPasswordRequest);
 }
