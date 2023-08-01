a4fc3db98d66363592150b54fd4fcc9a059ddbb7
GregBiglione <gregory.biglione81@gmail.com>
2023-08-01 19:27:32 +0200

    Added newPassword() in RemoteDataSource

--- lib/data/data_source/remote_data_source.dart
+++ lib/data/data_source/remote_data_source.dart
@@ -1,7 +1,10 @@
 import 'package:city_guide/data/request/login_request.dart';
+import 'package:city_guide/data/request/new_password_request.dart';
+import 'package:city_guide/data/response/forgot_password_response.dart';
 
 import '../response/response.dart';
 
 abstract class RemoteDataSource {
   Future<AuthenticationResponse> login(LoginRequest loginRequest);
+  Future<ForgotPasswordResponse> newPassword(NewPasswordRequest newPasswordRequest);
 }
