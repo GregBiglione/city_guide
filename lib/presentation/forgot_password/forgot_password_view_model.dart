7edd51f4bcdcef5443d1cd2b61a42a8f93a2c328
GregBiglione <gregory.biglione81@gmail.com>
2023-08-01 22:25:27 +0200

    Added ForgotPasswordViewModel

---
+++ lib/presentation/forgot_password/forgot_password_view_model.dart
@@ -0,0 +1,13 @@
+import 'package:city_guide/presentation/base/base_view_model.dart';
+
+class ForgotPasswordViewModel extends BaseViewModel {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+}
