5e5fc9b5972f2c7cdebe743ec43082841682dca1
GregBiglione <gregory.biglione81@gmail.com>
2023-08-03 01:53:55 +0200

    Added RegisterUseCaseInput

---
+++ lib/domain/usecase/register_use_case_input.dart
@@ -0,0 +1,11 @@
+class RegisterUseCaseInput {
+  String countryMobileCode;
+  String username;
+  String email;
+  String password;
+  String mobileNumber;
+  String profilePicture;
+
+  RegisterUseCaseInput(this.countryMobileCode, this.username, this.email,
+      this.password, this.mobileNumber, this.profilePicture);
+}
