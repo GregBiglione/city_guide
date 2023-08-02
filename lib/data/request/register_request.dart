295d2bf82c1baf0bd6a4a73df264951fc54446c3
GregBiglione <gregory.biglione81@gmail.com>
2023-08-03 01:27:44 +0200

    Added RegisterRequest object

---
+++ lib/data/request/register_request.dart
@@ -0,0 +1,11 @@
+class RegisterRequest {
+  String countryMobileCode;
+  String username;
+  String email;
+  String password;
+  String mobileNumber;
+  String profilePicture;
+
+  RegisterRequest(this.countryMobileCode, this.username, this.email,
+      this.password, this.mobileNumber, this.profilePicture);
+}
