292c5ad0f6b1788ce7034e2b25efe19ff59f2067
GregBiglione <gregory.biglione81@gmail.com>
2023-08-03 02:18:31 +0200

    Added RegisterViewModel

---
+++ lib/presentation/register/register_view_model.dart
@@ -0,0 +1,14 @@
+import 'package:city_guide/presentation/base/base_view_model.dart';
+
+class RegisterViewModel extends BaseViewModel {
+  @override
+  void start() {
+    // TODO: implement start
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+}
