6aefaaa39bcdf14176e4bfadee50ec1df18949c1
GregBiglione <gregory.biglione81@gmail.com>
2023-08-03 19:29:25 +0200

    Added RegisterViewModel & controllers in RegisterView

--- lib/presentation/register/register_view.dart
+++ lib/presentation/register/register_view.dart
@@ -1,3 +1,5 @@
+import 'package:city_guide/app/di/di.dart';
+import 'package:city_guide/presentation/register/register_view_model.dart';
 import 'package:flutter/material.dart';
 
 class RegisterView extends StatefulWidget {
@@ -8,6 +10,45 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
+  final RegisterViewModel _viewModel = instance<RegisterViewModel>();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _mobileNumberController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  _bind() {
+    _viewModel.start();
+    _usernameController.addListener(() =>
+        _viewModel.setUsername(_usernameController.text),
+    );
+    _mobileNumberController.addListener(() =>
+        _viewModel.setMobileNumber(_mobileNumberController.text),
+    );
+    _emailController.addListener(() =>
+        _viewModel.setEmail(_emailController.text),
+    );
+    _passwordController.addListener(() =>
+        _viewModel.setPassword(_passwordController.text),
+    );
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    _usernameController.dispose();
+    _mobileNumberController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
