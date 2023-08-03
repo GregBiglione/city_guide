390e4b7319ab9ca7e2dccc575795808ae10a38f4
GregBiglione <gregory.biglione81@gmail.com>
2023-08-03 02:39:15 +0200

    Added StreamController in RegisterViewModel

--- lib/presentation/register/register_view_model.dart
+++ lib/presentation/register/register_view_model.dart
@@ -1,6 +1,22 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:city_guide/presentation/base/base_view_model.dart';
 
 class RegisterViewModel extends BaseViewModel {
+  final StreamController _usernameStreamController = StreamController<String>
+      .broadcast();
+  final StreamController _mobileNumberStreamController = StreamController<String>
+      .broadcast();
+  final StreamController _emailStreamController = StreamController<String>
+      .broadcast();
+  final StreamController _passwordStreamController = StreamController<String>
+      .broadcast();
+  final StreamController _profilePictureStreamController = StreamController<File>
+      .broadcast();
+  final StreamController _isAllInputValidStreamController = StreamController<String>
+      .broadcast();
+
   @override
   void start() {
     // TODO: implement start
@@ -8,7 +24,12 @@ class RegisterViewModel extends BaseViewModel {
 
   @override
   void dispose() {
-    // TODO: implement dispose
+    _usernameStreamController.close();
+    _mobileNumberStreamController.close();
+    _emailStreamController.close();
+    _passwordStreamController.close();
+    _profilePictureStreamController.close();
+    _isAllInputValidStreamController.close();
     super.dispose();
   }
 }
