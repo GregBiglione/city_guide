ee61217192aae4c8027029fed53915d7fbf97aef
GregBiglione <gregory.biglione81@gmail.com>
2023-08-04 22:43:49 +0200

    Added BottomNavigationBar in main

--- lib/presentation/main/main_view.dart
+++ lib/presentation/main/main_view.dart
@@ -2,7 +2,9 @@ import 'package:city_guide/presentation/main/home_view.dart';
 import 'package:city_guide/presentation/main/notification_view.dart';
 import 'package:city_guide/presentation/main/search_view.dart';
 import 'package:city_guide/presentation/main/setting_view.dart';
+import 'package:city_guide/presentation/ressource/color_manager.dart';
 import 'package:city_guide/presentation/ressource/string_manager.dart';
+import 'package:city_guide/presentation/ressource/value_manager.dart';
 import 'package:flutter/material.dart';
 
 class MainView extends StatefulWidget {
@@ -19,7 +21,13 @@ class _MainViewState extends State<MainView> {
     const NotificationView(),
     const SettingView(),
   ];
-  String _title = AppString.home;
+  List<String> titles = [
+    AppString.home,
+    AppString.search,
+    AppString.notification,
+    AppString.setting,
+  ];
+  late String _title = AppString.home;
   int _currentIndex = 0;
 
   @override
@@ -32,6 +40,59 @@ class _MainViewState extends State<MainView> {
         ),
       ),
       body: views[_currentIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: ColorManager.black,
+              spreadRadius: AppSize.s1_5,
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          selectedItemColor: ColorManager.primary,
+          unselectedItemColor: ColorManager.grey,
+          onTap: switchIndex,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: ColorManager.white,
+                ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: ColorManager.white,
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.notifications,
+                color: ColorManager.white,
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings,
+                color: ColorManager.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  //----------------------------------------------------------------------------
+  // Switch index
+  //----------------------------------------------------------------------------
+
+  void switchIndex(selectedIndex){
+    setState(() {
+      _currentIndex = selectedIndex;
+      _title = titles[_currentIndex];
+    });
   }
 }
