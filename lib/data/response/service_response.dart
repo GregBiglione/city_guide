904f8ef4cbb862af9d1715aba400696f45d3b5e1
GregBiglione <gregory.biglione81@gmail.com>
2023-08-05 04:05:13 +0200

    Added ServiceResponse & ServiceResponse.g

---
+++ lib/data/response/service_response.dart
@@ -0,0 +1,21 @@
+import 'package:city_guide/data/response/response.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'service_response.g.dart';
+
+@JsonSerializable()
+class ServiceResponse {
+  @JsonKey(name: "id")
+  int? id;
+  @JsonKey(name: "title")
+  String? title;
+  @JsonKey(name: "image")
+  String? image;
+
+  ServiceResponse(this.id, this.title, this.image);
+
+  factory ServiceResponse.fromJson(Map<String, dynamic> json) =>
+      _$ServiceResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ServiceResponseToJson(this);
+}
