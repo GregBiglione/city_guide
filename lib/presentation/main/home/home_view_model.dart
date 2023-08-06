e5e4206022cbf0c1f13738c6d85201517a8a837a
GregBiglione <gregory.biglione81@gmail.com>
2023-08-06 21:48:00 +0200

    Added HomeViewModelInput & HomeViewModelOutput

--- lib/presentation/main/home/home_view_model.dart
+++ lib/presentation/main/home/home_view_model.dart
@@ -8,7 +8,7 @@ import 'package:rxdart/rxdart.dart';
 import '../../../domain/model/store.dart';
 import '../../base/base_view_model.dart';
 
-class HomeViewModel extends BaseViewModel {
+class HomeViewModel extends BaseViewModel with HomeViewModelInput, HomeViewModelOutput {
   final StreamController _bannerStreamController = BehaviorSubject<List<Banner>>();
   final StreamController _serviceStreamController = BehaviorSubject<List<Service>>();
   final StreamController _storeStreamController = BehaviorSubject<List<Store>>();
@@ -17,6 +17,7 @@ class HomeViewModel extends BaseViewModel {
 
   HomeViewModel(this._homeUseCase);
 
+  // Input ---------------------------------------------------------------------
   @override
   void start() {
     // TODO: implement start
@@ -29,4 +30,42 @@ class HomeViewModel extends BaseViewModel {
     _storeStreamController.close();
     super.dispose();
   }
+
+  @override
+  Sink get inputBanner => _bannerStreamController.sink;
+
+  @override
+  Sink get inputService => _serviceStreamController.sink;
+
+  @override
+  Sink get inputStore => _storeStreamController.sink;
+
+  // Output --------------------------------------------------------------------
+  @override
+  Stream<List<Banner>> get outputBanner => _bannerStreamController.stream
+      .map((banner) => banner);
+
+  @override
+  Stream<List<Service>> get outputService => _serviceStreamController.stream
+      .map((service) => service);
+
+  @override
+  Stream<List<Store>> get outputStore => _storeStreamController.stream
+      .map((store) => store);
+}
+
+// Input means order that view model will receive from view --------------------
+abstract class HomeViewModelInput {
+  // 3 sinks for stream --------------------------------------------------------
+  Sink get inputBanner;
+  Sink get inputService;
+  Sink get inputStore;
+}
+
+// Output means data/result that will be sent from view to view ----------------
+abstract class HomeViewModelOutput {
+  // 3 streams for validation --------------------------------------------------
+  Stream<List<Banner>> get outputBanner;
+  Stream<List<Service>> get outputService;
+  Stream<List<Store>> get outputStore;
 }
