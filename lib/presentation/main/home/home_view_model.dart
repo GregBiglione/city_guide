889725e06e9541360f7cd2c470762dbb89024d58
GregBiglione <gregory.biglione81@gmail.com>
2023-08-06 21:24:09 +0200

    Added StreamController in HomeViewModel

--- lib/presentation/main/home/home_view_model.dart
+++ lib/presentation/main/home/home_view_model.dart
@@ -1,8 +1,18 @@
-import 'package:city_guide/domain/usecase/home_usecase.dart';
+import 'dart:async';
 
+import 'package:city_guide/domain/model/banner.dart';
+import 'package:city_guide/domain/model/service.dart';
+import 'package:city_guide/domain/usecase/home_usecase.dart';
+import 'package:rxdart/rxdart.dart';
+
+import '../../../domain/model/store.dart';
 import '../../base/base_view_model.dart';
 
 class HomeViewModel extends BaseViewModel {
+  final StreamController _bannerStreamController = BehaviorSubject<List<Banner>>();
+  final StreamController _serviceStreamController = BehaviorSubject<List<Service>>();
+  final StreamController _storeStreamController = BehaviorSubject<List<Store>>();
+
   HomeUseCase _homeUseCase;
 
   HomeViewModel(this._homeUseCase);
@@ -14,7 +24,9 @@ class HomeViewModel extends BaseViewModel {
 
   @override
   void dispose() {
-    // TODO: implement dispose
+    _bannerStreamController.close();
+    _serviceStreamController.close();
+    _storeStreamController.close();
     super.dispose();
   }
 }
