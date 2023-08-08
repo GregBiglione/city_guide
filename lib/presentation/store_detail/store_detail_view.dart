ca09e285fe25e20ca7cf38af52c9c322ddb2d382
GregBiglione <gregory.biglione81@gmail.com>
2023-08-08 03:25:13 +0200

    Added _getContentWidget() in StoreDetailView

--- lib/presentation/store_detail/store_detail_view.dart
+++ lib/presentation/store_detail/store_detail_view.dart
@@ -1,3 +1,5 @@
+import 'package:city_guide/domain/model/detail_store.dart';
+import 'package:city_guide/presentation/common/state_renderer/state_renderer_implementer.dart';
 import 'package:city_guide/presentation/store_detail/store_detail_view_model.dart';
 import 'package:flutter/material.dart';
 
@@ -31,6 +33,31 @@ class _StoreDetailViewState extends State<StoreDetailView> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Scaffold(
+        body: StreamBuilder<FlowState>(
+          stream: _viewModel.outputState,
+          builder: (context, snapshot) {
+            return snapshot.data?.getWidgetScreen(
+                context,
+                _getContentWidget(),
+                    () {
+                  _viewModel.start();
+                }
+            ) ?? _getContentWidget();
+          },
+        ),
+      ),
+    );
   }
+
+  Widget _getContentWidget() => StreamBuilder<StoreDetail>(
+    stream: _viewModel.outputStoreDetail,
+    builder: (context, snapshot) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+      );
+    },
+  );
 }
