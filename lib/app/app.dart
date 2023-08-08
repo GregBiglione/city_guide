9660c4a23b85e2e7de13d1a2d24e85faf58b9e31
GregBiglione <gregory.biglione81@gmail.com>
2023-08-08 22:49:28 +0200

    Added location in App

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,9 @@
+import 'package:city_guide/app/app_preferences.dart';
 import 'package:city_guide/presentation/ressource/route_manager.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import '../presentation/ressource/theme_manager.dart';
+import 'di/di.dart';
 
 class MyApp extends StatefulWidget {
   int appState = 0;
@@ -15,9 +18,22 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final AppPreferences _appPreferences = instance<AppPreferences>();
+
+  @override
+  void didChangeDependencies() {
+    _appPreferences.getLocal().then((local) => {
+      context.setLocale(local)
+    });
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       debugShowCheckedModeBanner: false,
       onGenerateRoute: RouteGenerator.getRoute,
       initialRoute: Routes.splashRoute,
