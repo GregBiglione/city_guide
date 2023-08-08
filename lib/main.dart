4d6cc4b71c7cc81418532243ed2ca976d60b4c75
GregBiglione <gregory.biglione81@gmail.com>
2023-08-08 22:36:32 +0200

    Modified main with location

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:city_guide/app/di/di.dart';
+import 'package:city_guide/presentation/ressource/language_manager.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 
 import 'app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await initAppModule();
-  runApp(MyApp());
+  runApp(
+    EasyLocalization(
+        supportedLocales: const [
+          ENGLISH_LOCALE,
+          ARABIC_LOCALE,
+        ],
+        path: ASSETS_PATH_LOCALISATION,
+        child: Phoenix(child: MyApp()),
+    )
+  );
 }
