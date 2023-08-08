43c9e30304515cfcfafd0c543428626b248d95d9
GregBiglione <gregory.biglione81@gmail.com>
2023-08-08 02:27:12 +0200

    Added StoreDetailViewModelInput, StoreDetailViewModelOutput & StreamController in StoreDetailViewModel

--- lib/presentation/store_detail/store_detail_view_model.dart
+++ lib/presentation/store_detail/store_detail_view_model.dart
@@ -1,6 +1,18 @@
-import 'package:city_guide/presentation/base/base_view_model.dart';
+import 'dart:async';
 
-class StoreDetailViewModel extends BaseViewModel {
+import 'package:city_guide/domain/model/detail_store.dart';
+import 'package:city_guide/domain/usecase/store_detail_usecase.dart';
+import 'package:city_guide/presentation/base/base_view_model.dart';
+import 'package:rxdart/rxdart.dart';
+
+class StoreDetailViewModel extends BaseViewModel with StoreDetailViewModelInput,
+    StoreDetailViewModelOutput {
+  final StoreDetailUseCase _storeDetailUseCase;
+  final StreamController _dataStreamController = BehaviorSubject<StoreDetail>();
+
+  StoreDetailViewModel(this._storeDetailUseCase);
+
+  // Input ---------------------------------------------------------------------
   @override
   void start() {
     // TODO: implement start
@@ -8,7 +20,27 @@ class StoreDetailViewModel extends BaseViewModel {
 
   @override
   void dispose() {
-    // TODO: implement dispose
+    _dataStreamController.close();
     super.dispose();
   }
+
+  @override
+  Sink get inputStoreDetail => _dataStreamController.sink;
+
+  // Output --------------------------------------------------------------------
+  @override
+  Stream<StoreDetail> get outputStoreDetail => _dataStreamController.stream
+      .map((data) => data);
+}
+
+// Input means order that view model will receive from view --------------------
+abstract class StoreDetailViewModelInput {
+  // 1 sink for stream ---------------------------------------------------------
+  Sink get inputStoreDetail;
+}
+
+// Output means data/result that will be sent from view to view ----------------
+abstract class StoreDetailViewModelOutput {
+  // 1 stream for validation ---------------------------------------------------
+  Stream<StoreDetail> get outputStoreDetail;
 }
